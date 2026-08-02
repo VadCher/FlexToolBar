245c063463246c3ebfb10d1bacc26b869e22adcc
Vadim Cherkashin <vadcher@gmail.com>
2026-08-02 21:06:44 +0700

    feat(ui): add custom control Tab inheriting from HeaderedItemsControl for Avalonia 12

---
+++ src/FlexToolBar.Avalonia/Tab.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls.Primitives;
+
+namespace FlexToolBar.Avalonia;
+
+/// <summary>
+/// Represents a tab container control holding a collection of <see cref="FlexGroup"/> items.
+/// </summary>
+public class Tab : HeaderedItemsControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Tab"/> class.
+    /// </summary>
+    public Tab()
+    {
+    }
+}
