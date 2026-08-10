d33eb9aadaf9acefd6863b9ec1498b57ebbe742f
Vadim Cherkashin <vadcher@gmail.com>
2026-08-10 23:28:09 +0700

    styles edited

--- src/FlexToolBar.Avalonia/Controls/ToolBarSettingsFlexGroup.cs
+++ src/FlexToolBar.Avalonia/Controls/ToolBarSettingsFlexGroup.cs
@@ -1,30 +1,8 @@
-using System;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.LogicalTree;
 
 namespace FlexToolBar.Avalonia
 {
     public class ToolBarSettingsFlexGroup : FlexGroup
     {
-        private ToolBar? _parentToolBar;
-
-        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-        {
-            base.OnApplyTemplate(e);
-
-            _parentToolBar = this.FindLogicalAncestorOfType<ToolBar>();
-            
-            var resetBtn = e.NameScope.Find<Button>("PART_ResetStyleButton");
-            if (resetBtn != null)
-            {
-                resetBtn.Click += OnResetStyleClick;
-            }
-        }
-
-        private void OnResetStyleClick(object? sender, global::Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            _parentToolBar?.ResetToDefaultLayout();
-        }
     }
 }
